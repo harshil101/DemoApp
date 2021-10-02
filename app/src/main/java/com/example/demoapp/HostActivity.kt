145977ef84977360d50ed73d37319cd45a776ea8
Harshil <harshil101@outlook.com>
2021-10-02 23:20:26 +0530

    unused code removed

--- app/src/main/java/com/example/demoapp/HostActivity.kt
+++ app/src/main/java/com/example/demoapp/HostActivity.kt
@@ -11,8 +11,5 @@ class HostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 		binding = HostActivityBinding.inflate(layoutInflater)
 		setContentView(binding.root)
-		val navHostFragment =
-			supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-		val navController = navHostFragment.navController
     }
 }
